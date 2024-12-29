c29fc3a6e25a5d3adca23fc0f300d2e9dc3954ec
riku59 <123378420+riku59@users.noreply.github.com>
2024-12-29 16:27:36 +0100

    mise en place du site

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { browserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Projection</h1>
+    </div>
+  );
+};
+
+export default App;
