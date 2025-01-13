36d513716f7078c9bb2a7229f4d8bb2839de0ae8
riku59 <123378420+riku59@users.noreply.github.com>
2025-01-13 18:29:42 +0100

    ajout du début du style de la page bilan

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,42 +4,50 @@ import Footer from "../components/Footer";
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact">
       <Navigation />
       <h1>Contact</h1>
       <div className="formulaire_contact">
         <form action="#">
-          <label htmlFor="nom">Nom et prenom</label>
-          <input
-            type="text"
-            name="nom"
-            id="nom"
-            required
-            placeholder="Nom et prénom"
-          />
-          <label htmlFor="email">E-mail</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            required
-            placeholder="Entrez votre e-mail"
-          />
-          <label htmlFor="objet">Objet</label>
-          <input
-            type="text"
-            name="objet"
-            id="objet"
-            required
-            placeholder="Objet"
-          />
-          <label htmlFor="message">Message</label>
-          <textarea
-            name="message"
-            id="message"
-            cols="30"
-            placeholder="Entrez votre message"
-          ></textarea>
+          <div className="nom">
+            <label htmlFor="nom">Nom et prenom</label>
+            <input
+              type="text"
+              name="nom"
+              id="nom"
+              required
+              placeholder="Nom et prénom"
+            />
+          </div>
+          <div className="email">
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              placeholder="Entrez votre e-mail"
+            />
+          </div>
+          <div className="objet">
+            <label htmlFor="objet">Objet</label>
+            <input
+              type="text"
+              name="objet"
+              id="objet"
+              required
+              placeholder="Objet"
+            />
+          </div>
+          <div className="message">
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              cols="30"
+              placeholder="Entrez votre message"
+            ></textarea>
+          </div>
         </form>
       </div>
       <Footer />
