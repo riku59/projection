73394062947aa3e51ef84da807a12f4726a774c3
riku59 <123378420+riku59@users.noreply.github.com>
2025-01-22 16:00:48 +0100

    ajout d'un titre dans le iframe pour corriger l'erreur de build

--- src/pages/RendezVous.js
+++ src/pages/RendezVous.js
@@ -36,6 +36,7 @@ const RendezVous = () => {
         </div>
         <div className="map">
           <iframe
+            title="carte du lieu"
             className="lieu"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2524.0632116926263!2d2.2674260132698962!3d50.755861571533856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47dcf8c73283f923%3A0x7b20b8c20732a82a!2s16%20Rue%20des%20Mara%C3%AEchers%2C%2062500%20Saint-Omer!5e0!3m2!1sfr!2sfr!4v1737545372872!5m2!1sfr!2sfr"
             // width="600"
